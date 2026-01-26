3d0beb6dbed918c1922c67e2e86e136fb920317f
Emon Hossain <islamemon59@gmail.com>
2026-01-27 05:11:56 +0600

    navbar login register page added

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,8 +1,10 @@
+import { Navbar } from '@/components/Layout/Navbar';
 import React from 'react';
 
 const CommonLayout = ({children}: {children: React.ReactNode}) => {
     return (
         <div>
+            <Navbar/>
             {children}
         </div>
     );
---
+++ src/app/(commonLayout)/pricing/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PricingPage = () => {
+    return (
+        <div>
+            This is pricing page
+        </div>
+    );
+};
+
+export default PricingPage;
