c79381a3608f5d8304537960821bf0c619c8c2f5
Emon Hossain <islamemon59@gmail.com>
2026-01-29 00:25:25 +0600

    react form signup form added

--- src/app/(commonLayout)/register/page.tsx
+++ src/app/(commonLayout)/register/page.tsx
@@ -1,9 +1,11 @@
-const RegisterPage = () => {
-    return (
-        <div>
-            This is register page
-        </div>
-    );
-};
+import { SignupForm } from "@/components/authentication/signup-form"
 
-export default RegisterPage;
+export default function Page() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <SignupForm />
+      </div>
+    </div>
+  )
+}
