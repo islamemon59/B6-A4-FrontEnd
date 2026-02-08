eec8b1d65f46ec8a0effdb1254df9621d110acc2
Emon Hossain <islamemon59@gmail.com>
2026-02-08 15:22:57 +0600

    auth client file updated

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/auth`,
   fetchOptions: {
     credentials: "include",
   },
